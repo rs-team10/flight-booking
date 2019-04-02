8301604c8c592f01d0df6dc187645956bd4dba06
MRF <filipeng97@gmail.com>
2019-04-02 16:07:18 +0200

    Resolving

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import EditUser from './components/EditUser.vue'
+import EditAirline from './components/EditAirline.vue'
+import Hotels from './views/Hotels.vue'
 import vCarSearch from './views/vCarSearch.vue'
 import EditRcs from './components/EditRcs.vue'
-
 Vue.use(Router)
 
 export default new Router({
@@ -12,6 +14,21 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/editUser',
+      name: 'editUser',
+      component: EditUser
+    },
+    {
+      path: '/editAirline',
+      name: 'editAirline',
+      component: EditAirline
+    },
+    {
+      path: '/hotels',
+      name: 'hotels',
+      component: Hotels
     },
     {
       path: '/vCarSearch',
